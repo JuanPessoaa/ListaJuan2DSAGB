093855816a294ab1d51ba30f1713e6913a4065c7
JuanPessoaa <juan.pessoa.alves@gmail.com>
2026-03-26 22:32:56 -0300

    Esqueleto Ex6 Feito

---
+++ ListaJuan2DSAGB/Ex6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaJuan2DSAGB
+{
+    public partial class Ex6 : Form
+    {
+        public Ex6()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float vol, pi = 3.14159F, raio, alt;
+            raio = float.Parse(textBox1.Text);
+            alt = float.Parse(textBox2.Text);
+            vol = pi * (raio * raio) * alt;
+            label5.Text = Math.Round(vol, 2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label4.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
